36770317be4502af6d408b380c71a42cc48fa349
Nikesh Pakuwal <pakuwalnikesh@gmail.com>
2021-10-06 10:44:39 +0545

    Swagger implemented with solving errors

--- django_swagger/settings.py
+++ django_swagger/settings.py
@@ -64,6 +64,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': { # Adding this section should work around the issue.
+                'staticfiles' : 'django.templatetags.static',
+            },
         },
     },
 ]
